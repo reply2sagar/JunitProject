e4a0b2b05565d0b3fecde1d9aa7396241fb2edd6
localsagar <sagar@softpost.org>
2016-04-24 17:21:04 +1000

    Junit project

---
+++ src/main/java/junittests/App.java
@@ -0,0 +1,13 @@
+package junittests;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
