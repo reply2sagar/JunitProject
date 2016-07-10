308601a11c0f2ac97d91f95e4d377fc29024b0d7
sagar <sagar@softpost.com>
2016-07-10 17:39:44 +1000

    env variable in TeamCity

--- src/test/java/MathsTest.java
+++ src/test/java/MathsTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import java.util.Map;
+
 /**
  * Created by Sagar on 10-07-2016.
  */
@@ -15,5 +17,14 @@ public class MathsTest {
     public void test2(){
         System.out.println("Executing multiplication test");
         assert 12 == MathsClass.multiply(6,2);
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("Print all env variables");
+        Map<String, String> env = System.getenv();
+        for (String envName : env.keySet()) {
+            System.out.format("%s=%s%n", envName, env.get(envName));
+        }
     }
 }
